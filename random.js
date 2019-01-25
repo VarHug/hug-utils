33ee725b0c3620c8108fe37e7212fc712e08af15
VarHug <490641570@qq.com>
2019-01-25 20:26:50 +0800

    Feat:random:增加洗牌算法

--- random.js
+++ random.js
@@ -7,3 +7,20 @@
 export function random(m, n) {
   return (m + Math.random() * (n - m + 1)) | 0
 }
+
+/**
+ * 洗牌算法
+ * @export
+ * @param {Array} arr
+ * @returns {Array}
+ */
+export function shuffle(arr) {
+  let _arr = arr.slice()
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = random(0, i)
+    let temp = _arr[i]
+    _arr[i] = _arr[j]
+    _arr[j] = temp
+  }
+  return _arr
+}
