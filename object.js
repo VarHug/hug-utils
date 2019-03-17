9b8cc555d674bc11a6d233ad04bce2088978d400
VarHug <490641570@qq.com>
2019-03-17 14:30:15 +0800

    Feat:object:增加对象的一些方法

---
+++ object.js
@@ -0,0 +1,57 @@
+/**
+ * 获取Object的具体类型
+ * @param {Object} obj
+ * @returns {String} Object,Array,Function
+ */
+export function getObjectType(obj) {
+  return Object.prototype.toString.call(obj).replace(/\[object (\w+)\]/,"$1")
+}
+
+/**
+ * 获取对象的长度
+ * @param {Object} obj
+ * @returns {boolean}
+ */
+export function getObjectLength(obj) {
+  if (getObjectType(obj) === 'Object') {
+    return Object.keys(obj).length
+  }
+}
+
+/**
+ * 判断两个对象是否相等
+ * @param {Object} obj1
+ * @param {Object} obj2
+ * @returns {boolean}
+ */
+export function equalObject(obj1, obj2) {
+  let type1 = getObjectType(obj1)
+  let type2 = getObjectType(obj2)
+  if (type1 !== type2) {
+    return false
+  }
+  let len1 = Object.keys(obj1).length
+  let len2 = Object.keys(obj2).length
+  if (len1 !== len2) {
+    return false
+  }
+  let stack = []
+  stack.push(obj1, obj2)
+  while (stack.length) {
+    let item1 = stack.shift()
+    let item2 = stack.shift()
+    let keys = Object.keys(item1)
+    let isEqual = keys.every((key) => {
+      if (item1[key] instanceof Object) {  // 忽略函数
+        stack.push(item1[key], item2[key])
+        return true
+      } else {
+        return item1[key] === item2[key]
+      }
+    })
+    if (!isEqual) {
+      return false
+    }
+  }
+  return true
+}
