865fb921f7fc4ffb29cd7df7815900eaaa47c648
VarHug <490641570@qq.com>
2021-02-02 16:33:54 +0800

    feat: 🎸 add adjust

---
+++ src/List/adjust.js
@@ -0,0 +1,27 @@
+import { _curry } from '../internal';
+
+/**
+ * @description 将数组中指定索引处的值替换为经函数变换的值。
+ * @sig Number -> (a -> a) -> [a] -> [a] (curry)
+ * @param {number} idx
+ * @param {Function} fn
+ * @param {Array} list
+ * @returns {Array}
+ * @example
+ *      adjust(1, toUpper, ['a', 'b', 'c', 'd']);      //=> ['a', 'B', 'c', 'd']
+ *      adjust(-1, toUpper, ['a', 'b', 'c', 'd']);     //=> ['a', 'b', 'c', 'D']
+ */
+const adjust = (idx, fn, list) => {
+  if (idx < -list.length || idx >= list.length) {
+    return list;
+  }
+  const start = idx < 0 ? list.length : 0;
+  const _idx = start + idx;
+  const _list = list.slice();
+  _list[_idx] = fn(_list[_idx]);
+  return _list;
+};
+
+const curryAdjust = _curry(adjust);
+
+export default curryAdjust;
