7d848ab788aa2a43709bdfac28576cc154f7fdf1
VarHug <490641570@qq.com>
2021-02-02 17:07:42 +0800

    feat: 🎸 add simpleAdd

---
+++ src/Math/simpleAdd.js
@@ -0,0 +1,15 @@
+import { _curry } from '../internal';
+
+/**
+ * @description 简单两数相加的加法
+ * @sig Number -> Number -> Number
+ * @param {number} a
+ * @param {number} b
+ */
+const simpleAdd = (a, b) => {
+  return Number(a) + Number(b);
+};
+
+const currySimpleAdd = _curry(simpleAdd);
+
+export default currySimpleAdd;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 export * from './Function';
+export * from './List';
+export * from './Math';
