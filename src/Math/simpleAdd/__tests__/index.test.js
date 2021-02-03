ec201ba2c593f91196ccb69a5839426b10d37dce
VarHug <490641570@qq.com>
2021-02-03 14:52:57 +0800

    feat: 🎸 添加单元测试

--- src/Math/simpleAdd/__tests__/index.test.js
+++ src/Math/simpleAdd/__tests__/index.test.js
@@ -1,5 +1,8 @@
 import simpleAdd from '../index';
 
-test('simpleAdd 1 + 2 to equal 3', () => {
+test('simpleAdd', () => {
   expect(simpleAdd(1, 2)).toBe(3);
+  expect(simpleAdd(2, 3)).toBe(5);
+  expect(simpleAdd(5)(10)).toBe(15);
+  expect(simpleAdd(3)(4)).toBe(7);
 });
