c2e660dbf9f45b2248783b1d708c78535a1fd4ed
VarHug <490641570@qq.com>
2021-02-08 17:09:49 +0800

    refactor: 💡 keys用语法糖替换type

--- src/Object/keys/index.js
+++ src/Object/keys/index.js
@@ -1,5 +1,4 @@
-import { TypeEnum } from '../../Enum';
-import { type } from '../../Type';
+import { isObject, isArray } from '../../Type';
 
 /**
  * @description 返回给定对象所有可枚举的、自身属性的属性名组成的列表。注意，不同 JS 运行环境输出数组的顺序可能不一致。
@@ -8,8 +7,7 @@ import { type } from '../../Type';
  * @returns {Array}
  */
 const keys = (obj) => {
-  const _type = type(obj);
-  if (_type === TypeEnum.OBJECT || _type === TypeEnum.ARRAY) {
+  if (isObject(obj) || isArray(obj)) {
     return Object.keys(obj);
   }
   return [];
