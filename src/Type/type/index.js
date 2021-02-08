54fc5f477d642e2c3256c7aa8da780bd86620e56
VarHug <490641570@qq.com>
2021-02-08 11:54:14 +0800

    refactor: 💡 type类型使用常量替换

--- src/Type/type/index.js
+++ src/Type/type/index.js
@@ -1,3 +1,5 @@
+import { TypeEnum } from '../../Enum';
+
 /**
  * @description 用一个单词来描述输入值的（原生）类型，返回诸如 'Object'、'Number'、'Array'、'Null' 之类的结果。不区分用户自定义的类型，统一返回 'Object'。
  * @sig (* -> {*}) -> String
@@ -6,9 +8,9 @@
  */
 const type = (val) => {
   if (val === undefined) {
-    return 'Undefined';
+    return TypeEnum.UNDEFINED;
   } else if (val === null) {
-    return 'Null';
+    return TypeEnum.NULL;
   }
   return Object.prototype.toString.call(val).slice(8, -1);
 };
