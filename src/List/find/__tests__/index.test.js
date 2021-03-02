fb88e26f4732f7c55acc73b2f5e0f5808c585f8a
VarHug <490641570@qq.com>
2021-03-02 18:02:58 +0800

    refactor: 💡 propEq改写find测试用例

--- src/List/find/__tests__/index.test.js
+++ src/List/find/__tests__/index.test.js
@@ -1,9 +1,8 @@
 import find from '../index';
+import { propEq } from '../../../Object';
 
 test('find', () => {
   const xs = [{ a: 1 }, { a: 2 }, { a: 3 }];
-  const isAEqualTwo = ({ a }) => a === 2;
-  const isAEqualFour = ({ a }) => a === 4;
-  expect(find(isAEqualTwo)(xs)).toEqual({ a: 2 });
-  expect(find(isAEqualFour)(xs)).toBe(undefined);
+  expect(find(propEq('a', 2))(xs)).toEqual({ a: 2 });
+  expect(find(propEq('a', 4))(xs)).toBe(undefined);
 });
